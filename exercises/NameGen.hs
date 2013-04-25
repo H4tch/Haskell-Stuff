9347a5c1e52e0bf727b40b0cf0d9a78678885809
Dan Hatch <danhatch333@gmail.com>
2013-04-24 19:55:56 -0500

    Fixed up and tweaked NameGen.hs.

--- exercises/NameGen.hs
+++ exercises/NameGen.hs
@@ -5,24 +5,25 @@ import Control.Monad.IO.Class
 
 vowels = "aeiouy"
 
-getVowel (s, gen) = (s ++ (vowels !! index), gen') where (index, gen')= randomR (0,5) gen
+getVowel (s, gen) = (s ++ [vowels !! index], gen') where (index, gen')= randomR (0,5) gen
 
 getConsonant (s, gen) =
   let c = ['b'..'z'] !! index
-  in if [c] `elem` vowels
+  in if c `elem` vowels
     then getConsonant (s, gen')
     else (s ++ [c], gen')
   where (index, gen') = randomR (0,24) gen
 
 genVowelPart (s, gen) =
-  let (n, gen') = randomR ((1::Int),4) gen
+  let (n, gen') = randomR ((1::Int),8) gen
       (s', gen'') = getVowel(s, gen')
-  in if n == (1::Int) then (s', gen'') else getConsonant(s', gen'')
-  
+  in if n == (1::Int) then getVowel(s', gen'') else (s', gen'')
+
+
 genConsonantPart (s, gen) =
-  let (n, gen') = randomR ((1::Int),4) gen
+  let (n, gen') = randomR ((1::Int),14) gen
       (s', gen'') = getConsonant(s, gen')
-  in if n == (1::Int) then (s', gen'') else getConsonant(s', gen'')
+  in if n == (1::Int) then getConsonant(s', gen'') else (s', gen'')
 
 genName (s, gen) =
     let (rand,gen') = randomR (1 :: Int ,2) gen
@@ -31,13 +32,13 @@ genName (s, gen) =
            _ -> genConsonantPart . genVowelPart . genConsonantPart . genVowelPart $ genConsonantPart ("", gen')
     in (name, gen'')
 
-generate gen = do
-    (name, gen') <- generate ("", gen)
-    putStrLn $ (name) ++ "    " ++ (reverse name)
-    return gen'
 
 main = do
     (rand,gen) <- randomR (1 :: Int ,2) `fmap` getStdGen
-    generate gen
+    let (name, _) = genName ("", gen)
+    if (length name) < (8 :: Int)
+    	then putStrLn $ (name) ++ "\t" ++ (reverse name)
+    	else putStrLn $ (name) ++ " " ++ (reverse name)
+    
 
 
